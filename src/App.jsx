9ec30b193f5c7de2ea7635ff2d992f72d1a801bd
Oscar Mesejo <oscar.mesejo@gmail.com>
2023-08-22 15:53:32 +0200

    iteration 1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
+import { useState } from "react";
 import "./App.css";
+import contactsArray from "./contacts.json";
 
 function App() {
+
+  const fiveContacts = contactsArray.slice(0,5);
+  const [contacts, setContacts] = useState(fiveContacts);
+  
+
   return (
-    <div className="App">
-      <h1>LAB | React IronContacts</h1>
+    <div>
+      <h1>IronContacts</h1>
+      <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+        </tr>
+
+        {contacts.map((contactsArray)=>(
+          <tr className="contact-row" key={contacts._id}>
+          <td><img src={contactsArray.pictureUrl} width="100px"alt="" /></td>
+          <td>{contactsArray.name}</td>
+          <td>{contactsArray.popularity}</td>
+
+          {contacts.hasOscar ? <p>:trophy:</p> : <p></p>}
+        </tr>
+        ))}
+        
+      </table>
     </div>
   );
 }
